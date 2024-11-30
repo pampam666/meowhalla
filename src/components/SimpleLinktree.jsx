c69acd36bf6738781f0aa213851adbad4954201d
rizzdev101 <pramono.23340@mhs.unesa.ac.id>
2024-11-30 17:03:48 +0700

    perubahan 2

--- src/components/SimpleLinktree.jsx
+++ src/components/SimpleLinktree.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function SimpleLinktree() {
   const links = [
     { id: 1, label: "Instagram", url: "https://www.instagram.com/pampam.______/" },
-    { id: 2, label: "LinkedIn", url: "https://linkedin.com" },
-    { id: 3, label: "Twitter", url: "https://twitter.com" },
-    { id: 4, label: "GitHub", url: "https://github.com" },
-    { id: 5, label: "Blog", url: "https://yourblog.com" },
+    { id: 2, label: "Twitter", url: "https://x.com/Pampam________" },
+    { id: 3, label: "GitHub", url: "https://github.com/pampam666" },
+    { id: 4, label: "Agency", url: "https://www.instagram.com/brixdigital.official/" },
+    { id: 5, label: "Portofolio", url: "https://linktreepampam.my.canva.site/portofolio-pramono"}
   ];
 
   return (
@@ -14,12 +14,12 @@ function SimpleLinktree() {
       {/* Profile Section */}
       <div className="linktree-header">
         <img
-          src="https://i.pinimg.com/736x/15/03/e5/1503e59e9a80f3b5b7595deaaba12d9d.jpg"
+          src="https://i.pinimg.com/280x280_RS/26/0d/3d/260d3d3d57eb36ea9bc3a129bd50122b.jpg"
           alt="Avatar"
           className="avatar"
         />
         <h1 className="linktree-title">Pramono Tirta Luhung</h1>
-        <p className="linktree-subtitle">Mahasiswa luar biasa</p>
+        <p className="linktree-subtitle">Mahasiswa biasa prodi Bisnis Digital</p>
       </div>
 
       {/* Links Section */}
